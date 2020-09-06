9385663cff355e79477bded08d11f52be3c88a3c
CandyDaniele <daniele_oli_lucas@hotmail.com>
2020-09-06 13:33:45 -0300

    teste cors 3

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://desafio4-front.herokuapp.com/*',
+    origin: 'https://desafio4-front.herokuapp.com',
   })
 );
 
